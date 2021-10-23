55325f5d1733eed09fb1383cb578b8291121560b
blackeye18 <panmavrom@gmail.com>
2021-10-23 13:17:43 -0700

    H function Ready

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,210 @@
 #include <cctype>
 #include <fstream>
 #include <sstream>
+#include <random>
+#include <chrono>
+#include <map>
+
 using namespace std;
+
+#define RLIMIT 100
+#define PNUM 4294967291
+#define W 4//meta3i 0 kai 6
+
 
 class vec
 {
 public:
-    string name;
+    string name;//to id ths grammhs-dianismatos
     vector <int> coord;
 };
 
 
+double normal_dist_generator(void)
+    {
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<float> d{0.0,1.0};
+    map<int, int> hist{};
+    return round(d(gen));
+    }
 
+
+class node
+    {
+    public:
+        long long hashvalue;
+        int id;
+        vec* vect;
+        node* next;
+        ~node(void);
+    };
+node::~node()
+    {
+    if(vect!=NULL)
+        {
+        delete vect;
+        vect=NULL;
+        }
+    if(next!=NULL)
+        {
+        delete next;
+        next=NULL;
+        }
+    }
+
+class hashtable
+    {
+    protected:
+        node* buckets;//array me ta buckets
+        int bucket_count;//ari8mos buckets
+        int total_nodes;
+        int initialized;
+    public:
+        hashtable(void); // plithos bucket
+        void hashtable_init(int);
+        int hashtable_insert(vec* , long long );//eisagwgh 
+        ~hashtable(void);
+    };
+
+hashtable::hashtable()
+    {
+    initialized=0;
+    }
+void hashtable::hashtable_init(int bnum)
+    {
+    buckets= new node[bnum];
+    bucket_count=bnum;
+    total_nodes=0;
+    initialized=1;
+    }
+
+int hashtable:: hashtable_insert(vec* nvec, long long hvalue)
+    {
+    if(not initialized){return 1;}
+    total_nodes++;
+    int id=hvalue % bucket_count;
+    if (buckets[id].vect==NULL)
+        {
+        buckets[id].hashvalue=hvalue;
+        buckets[id].id=id;
+        buckets[id].vect=nvec;
+        buckets[id].next=NULL;
+        }
+    else
+        {
+        node* tempn= new node;
+        tempn->hashvalue=hvalue;
+        tempn->id=id;
+        tempn->vect=nvec;
+        tempn->next=buckets[id].next;
+        buckets[id].next=tempn;
+        }
+    return 0;
+    }
+
+hashtable::~hashtable()
+    {
+    delete[] buckets;
+    buckets=NULL;
+    }
+class Lhashtables
+    {
+    protected:
+        hashtable* Lhtables;//array me ta hashtables
+        int L;//number of hashtables;
+        int d;//dimensions
+        int k;
+        vector<vector<vector<double>>> v;
+        vector<vector<double>> t;
+        vector<vector<int>> r;
+    public:
+        Lhashtables(int,int,int);
+        void Hashfun_init(void);
+        int lsh_start(int,vec*);
+        int lsh_continue(int,int,vec*);
+        //~Lhashtables(void);
+
+    };
+
+Lhashtables::Lhashtables(int lh,int di,int ki)
+    {
+    Lhtables= new hashtable[lh];
+    L=lh;
+    d=di;
+    k=ki;
+    }
+void Lhashtables::Hashfun_init(void)        
+    {
+    v.resize(L,vector<vector<double> >(k,vector<double>(d)));
+
+    t.resize(L,vector<double>(k));
+
+    r.resize(L,vector<int>(k));
+
+    unsigned seed=std::chrono::steady_clock::now().time_since_epoch().count();
+    default_random_engine e(seed);
+
+    std::uniform_int_distribution<int>  distrR(-RLIMIT,RLIMIT);
+    std::uniform_real_distribution<double>  distrT(0.0,W);
+
+
+    for (int Li = 0; Li < L; ++Li)
+        {
+        for (int ki = 0; ki < k; ++ki)
+            {
+            t[Li][ki]=distrT(e);    //cout<<"t "<<t[Li][ki]<<endl;
+            r[Li][ki]=distrR(e);    //cout<<"r "<<r[Li][ki]<<endl;
+
+            for (int di = 0; di < d; ++di)
+                {
+                v[Li][ki][di]=normal_dist_generator();
+                }
+            cout<<endl;
+            }
+        }
+
+    }
+
+int h_function(vector<int> p,vector<double> v,double t,int d){
+    double in_prod;
+    in_prod=inner_product(p.begin(),p.end(),v.begin(),0);
+    in_prod+=t;
+    double sum=in_prod/W;
+    int h=floor(sum);
+    return h;
+
+}
+int Lhashtables:: lsh_continue(int no_of_ht,int no_of_vectors, vec* nvectors){
+    int h_return;
+    int h[this->k];
+    for(int i=0;i<no_of_vectors;i++){
+        for(int ki=0;ki<this->k;ki++){
+
+            h_return=h_function(nvectors[i].coord,this->v[no_of_ht][ki],this->t[no_of_ht][ki],this->d);
+            h[ki]=h_return;
+            cout<<"H Function Return:"<<h[ki]<<endl;
+        }
+    }
+    return 0;
+}
+
+int Lhashtables:: lsh_start(int no_of_vectors,vec *nvectors){
+    this->Hashfun_init();
+    int ret=0;
+    for(int i=0;i<L;i++){
+        ret=this->lsh_continue(i,no_of_vectors,nvectors);
+        if(ret!=0){
+            cout<<"Something went wrong with lsh_continue"<<endl;
+            return-1;
+        }
+
+    }
+
+
+    return 0;
+
+}
 
 //elegxoume an to # twn arguments einai swsto
 int argsOK(int argc, char *argv[])
@@ -112,100 +305,7 @@ int input_handler(int argc, char *argv[],int* k, int* L, int* N,int* R,char (&in
     return 0;
 }
 
-/*
-//diavazei to plithos dianismatwn kai to plithos twn syntetagmenwn tous
-int get_N_of_input(int* no_of_vectors,int* no_of_coordinates,char input_file[256]){
-    //printf("input_file: %s",input_file);
 
-    (*no_of_vectors)=0;//
-    (*no_of_coordinates)=0;//ksekiname apo -1 giati metrame kai to item_id san sydetagmenh
-    FILE *fd;//anoigoume to arxeio, an den yparxei epistrefei -1 alliws  synexizei
-    fd=fopen(input_file,"r");
-    if(fd==NULL)
-        return -1;
-    char getch;
-    char prevchar;
-    getch=fgetc(fd);//diavazoume xarakthra xarakthra to arxeio
-    while(1){
-        if(getch==EOF)
-            break;
-        if(getch=='\n'){//an exoume allagh grammhs tote pame sto epomeno dianisma
-            if(isspace(prevchar)&& (*no_of_vectors)==0)
-                (*no_of_coordinates)--;
-            (*no_of_vectors)++;
-        }
-        else if ((getch=='\t'&& (*no_of_vectors)==0 )|| (getch==' '&& (*no_of_vectors)==0) ){//afou ola ta dianismata exoun idio plithos syntetagmenwn, metrame mono tou prwtou
-            
-            (*no_of_coordinates)++;
-            
-        }
-        prevchar=getch;
-        getch=fgetc(fd);
-    }
-    fclose(fd);
-
-    return 0;
-
-}
-
-
-void create_vectors(vec* nvectors,int no_of_vectors,char input_file[256],int no_of_coordinates){
-    //for(int i=0;i<no_of_vectors;i++){
-      //  nvectors[i].coord.push_back(i);
-    //}
-    string concat="";
-                    //cout<<"aaaaa"<<concat;
-
-    char getch;
-    int i,j,k,l;
-    int count=0;
-    int id_flag=1;
-        FILE *fd;
-        fd=fopen(input_file,"r");
-        if(fd==NULL){
-            cout<<"Problem while opening file!"<<endl;
-        }
-
-        getch=fgetc(fd);
-        char prevchar;
-        while(1){
-        if(getch==EOF)
-            break;
-        if(getch=='\n'){//an exoume allagh grammhs tote pame sto epomeno dianisma
-            //cout<<"prevchar:"<<prevchar<<endl;
-            //if((concat.empty()))
-                //nvectors[count].coord.push_back(stoi(concat));
-            concat="";
-            (count)++;
-            id_flag=1;
-        }
-        else if (getch=='\t' || getch==' '){
-            //if(prevchar!=' '){    
-                if(id_flag==1){
-                    id_flag=0;
-                    nvectors[count].name=concat;
-                    concat="";
-
-                }
-                else{
-                    //cout<<"aaaaa"<<concat;
-                    nvectors[count].coord.push_back(stoi(concat));
-                    concat="";
-                }
-           // }       
-        }else{
-            concat+=getch;
-            
-        }
-        prevchar=getch;
-        getch=fgetc(fd);
-
-        
-    }
-
-
-}
-*/
 
 vec* open_and_create_vectors(char input_file[256],int* no_of_coordinates,int *no_of_vectors){
     //cout<<"aaa1";
@@ -256,6 +356,7 @@ vec* open_and_create_vectors(char input_file[256],int* no_of_coordinates,int *no
                 }
                 else
                     nvectors[counter].coord.push_back(stoi(tok));
+                    
             }
         counter++;
     }
@@ -263,6 +364,22 @@ vec* open_and_create_vectors(char input_file[256],int* no_of_coordinates,int *no
     return nvectors;
             
 }
+
+void print_vectors(vec *nvectors,int no_of_vectors,int no_of_coordinates){
+    for(int i=0;i<no_of_vectors;i++){
+        cout<<"Dianisma :"<<i<<endl;
+        for(int j=0;j<no_of_coordinates;j++){
+        //for(auto j: vector[i].coord){
+            //cout<<j<<' ';
+            if(j==0)
+                cout<<"Name-id: "<<nvectors[i].name<<endl;
+            
+            cout<<nvectors[i].coord[j]<<" ";
+        }
+    }
+    cout<<endl;
+}
+
 
 
 
@@ -290,26 +407,12 @@ int main(int argc, char *argv[]){
     if(nvectors==NULL)
         return -1;
     printf("no_of_vectors: %d, no_of_coordinates: %d\n",no_of_vectors,no_of_coordinates);
+   // print_vectors(nvectors,no_of_vectors,no_of_coordinates);
+    Lhashtables lht(L,no_of_coordinates,k);
+    lht.lsh_start(no_of_vectors,nvectors);
+   
+cout<<"den krasaraaaa"<<endl;
 
-
-    //vec vector[no_of_vectors];
-  // create_vectors(vector,no_of_vectors,input_file,no_of_coordinates);
-    int j;
-    //cout.flush();
-    /*
-    for(int i=0;i<no_of_vectors;i++){
-        cout<<"Dianisma :"<<i<<endl;
-        for(j=0;j<no_of_coordinates;j++){
-        //for(auto j: vector[i].coord){
-            //cout<<j<<' ';
-            if(j==0)
-                cout<<"Name-id: "<<nvectors[i].name<<endl;
-            
-            cout<<nvectors[i].coord[j]<<" ";
-        }
-    }
-
-*/
    delete [] nvectors;
 		
 }
